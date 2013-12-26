ddcd95a4aee2874c2afd31f76f9d82f0addb89cf
Ricky <43517912@qq.com>
2013-12-26 21:24:11 +0800

    login

--- WebContent/src/login.js
+++ WebContent/src/login.js
@@ -4,4 +4,17 @@
  * @author: Ricky
  */
 
-
+$("form").submit(function(e) {
+	var username = $.trim($("#username").val()),
+		password = $.trim($("#password").val()),
+		tip = $(".tip");
+	if (username == "") {
+		tip.text("请输入用户名").show().fadeOut(1500);
+		e.preventDefault();
+		return;
+	}
+	if (password == "") {
+		tip.text("请输入密码").show().fadeOut(1500);
+		e.preventDefault();
+	}
+});
