7c87d85616162cd84e4ad49e067d208cf1a51a74
saturntu <saturntu@163.com>
2013-12-31 09:23:48 +0800

    upgrade .gitignore

--- src/cn/com/ecrf/trq/utils/AjaxReturnValue.java
+++ src/cn/com/ecrf/trq/utils/AjaxReturnValue.java
@@ -1,8 +1,12 @@
 package cn.com.ecrf.trq.utils;
 
-public class ReturnUtils {
+import java.util.Map;
+
+public class AjaxReturnValue {
 	public static final String success = "success";
 	public static final String errorMsg = "errorMsg";
 	public static final String total = "total";
 	public static final String data = "data"; 
+	
+	
 }
